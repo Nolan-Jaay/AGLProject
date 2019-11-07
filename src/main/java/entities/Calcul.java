60d9edbb4f753f9bdb6da68b742e0ddc3344885c
Nolan-Jaay <lzeu95@hotmail.com>
2019-11-07 11:02:33 +0000

    Premier commit du project AGLProject

---
+++ src/main/java/entities/Calcul.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Calcul {
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	
+	public int multiplication(int a,int b) {
+		return a*b;
+	}
+
+}
---
+++ src/test/java/test/TestCalcul.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import entities.Calcul;
+
+public class TestCalcul {
+	private Calcul subject;
+	
+	@Before
+	public void setup() {
+		subject=new Calcul();
+	}
+
+	@Test
+	public void testAdd() {
+		assertEquals(6, subject.addition(3, 3));
+	}
+	
+	@Test
+	public void testAdd1() {
+		assertNotEquals(8, subject.addition(2, 5));
+	}
+	
+	@Test
+	public void testAdd2() {
+		assertEquals(0, subject.addition(-3, 3));
+	}
+	
+	@Test
+	public void testMul() {
+		assertEquals(6, subject.multiplication(2, 3));
+	}
+	
+	@Test
+	public void testMul1() {
+		assertNotEquals(8, subject.multiplication(-2, 5));
+	}
+	
+	
+
+}
